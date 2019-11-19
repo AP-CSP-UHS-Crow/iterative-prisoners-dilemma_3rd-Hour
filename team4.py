09017804b16ab0d61b7dff69bc83a7b135369eb3
ETHANKLEINE <57766176+ETHANKLEINE@users.noreply.github.com>
2019-11-19 11:09:46 -0600

    our updated and perfected strategy to not suck and win
    jkbjdLA ... GREATNESS AWAITS

--- team4.py
+++ team4.py
@@ -7,7 +7,7 @@
 ####
 
 team_name = 'SwagManz' # Only 10 chars displayed.
-strategy_name = 'not sucking'
+strategy_name = 'not sucking.'
 strategy_description = 'just dont suck... quite simple really'
     
 def move(my_history, their_history, my_score, their_score):
@@ -17,7 +17,19 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if (my_score > their_score) and (their_history =='c'):
+        return 'b'
+    if (my_score < their_score) and (their_history =='c'):
+        return 'b'
+    if (my_score > their_score) and (their_history =='b'):
+        return ' b'
+    if (my_score < their_score) and (their_history =='b'):
+        return 'c'
+    if (my_score == their_score) and (their_history =='b'):
+        return 'c'
+    if (my_score == their_score) and (their_history =='c'):
+        return 'b'
+        
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +38,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -39,10 +51,10 @@ def test_move(my_history, their_history, my_score, their_score, result):
         return True
     else:
         print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
+            ", ".join(["'"+my_history+"'", "'"+str(their_history)+"'",
                        str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
+            ") returned " + "'" + str(real_result) + "'" +
+            " and should have returned '" + str(result) + "'")
         return False
 
 if __name__ == '__main__':
@@ -50,9 +62,9 @@ if __name__ == '__main__':
     # Test 1: Betray on first move.
     if test_move(my_history='',
               their_history='', 
-              my_score=0,
+              my_score = 0,
               their_score=0,
-              result='b'):
+              result='c'):
          print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
